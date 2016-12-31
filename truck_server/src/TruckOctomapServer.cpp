5f7ffec377c91f0c29df5535fc37232daa0607dd
KahnShi <shifan@jsk.imi.i.u-tokyo.ac.jp>
2016-12-31 23:26:15 +0900

    Fix bug of new gap.

--- truck_server/src/TruckOctomapServer.cpp
+++ truck_server/src/TruckOctomapServer.cpp
@@ -72,7 +72,7 @@ void TruckOctomapServer::WriteVehicleOctree(int type, Pose6D rot_mat)
   else if (type == 2)
     {
       base_size[0] = 10.0f; base_size[1] = 2.4f; base_size[2] = 1.4f;
-      roof_size[0] = 2.0f; roof_size[1] = 2.4f; roof_size[2] = 1.2f;
+      roof_size[0] = 2.1f; roof_size[1] = 2.4f; roof_size[2] = 1.2f;
       cargo_size[0] = 7.4f; cargo_size[1] = 2.4f; cargo_size[2] = 3.0f;
     }
 
@@ -103,6 +103,9 @@ void TruckOctomapServer::WriteVehicleOctree(int type, Pose6D rot_mat)
   else{
     cargo_offset[0] = -base_size[0]/2.0f; cargo_offset[1] = -cargo_size[1]/2.0f; cargo_offset[2] = base_size[2];
   }
+
+
+  // Judge whether start point is on boarder, in case octo map give seperate thin plannar.
   for (int i = 0; i < 3; ++i)
     {
       int res_100 = int(m_res*100);
@@ -149,8 +152,7 @@ void TruckOctomapServer::WriteVehicleOctree(int type, Pose6D rot_mat)
     }
   }
 
-  point3d end_pt(0, 0, 0);
-  //m_octree->updateNode(end_pt, true);
+
   m_octree->prune();
   m_octree->prune();
 
@@ -161,9 +163,12 @@ void TruckOctomapServer::WriteVehicleOctree(int type, Pose6D rot_mat)
 void TruckOctomapServer::WriteUavSafeBorderOctree(int type, Pose6D rot_mat)
 {
   int roof[3], base[3], cargo[3];
+  int roof_origin[3], base_origin[3], cargo_origin[3];
   float roof_offset[3], base_offset[3], cargo_offset[3];
   float roof_size[3], base_size[3], cargo_size[3];
-  float uav_safety_margin_size[3] = {0.6, 0.6, 0.2}, uav_safety_margin[3];
+  // For uav safety margin
+  float uav_safety_margin_size[3] = {2*0.6f, 2*0.6f, 0.2f}, uav_safety_margin[3];
+
   // truck for challenge
   if (type == 0)
     {
@@ -186,72 +191,92 @@ void TruckOctomapServer::WriteUavSafeBorderOctree(int type, Pose6D rot_mat)
       cargo_size[0] = 7.4f; cargo_size[1] = 2.4f; cargo_size[2] = 3.0f;
     }
 
+  // For uav safety margin
+  for (int i = 0; i < 3; ++i){
+    base_origin[i] = (int)round(base_size[i]/m_res);
+    roof_origin[i] = (int)round(roof_size[i]/m_res);
+    cargo_origin[i] = (int)round(cargo_size[i]/m_res);
+
+    base_size[i] += uav_safety_margin_size[i];
+    roof_size[i] += uav_safety_margin_size[i];
+    if (cargo_size[0] > 0.1)
+      cargo_size[i] += uav_safety_margin_size[i];
+  }
+
   for (int i = 0; i < 3; ++i){
     base[i] = (int)round(base_size[i]/m_res);
     roof[i] = (int)round(roof_size[i]/m_res);
     cargo[i] = (int)round(cargo_size[i]/m_res);
-    uav_safety_margin[i] = (int)round(uav_safety_margin_size[i]/m_res);
   }
-  base_offset[0] = 0.0f; base_offset[1] = 0.0f; base_offset[2] = base_size[2]/2.0f;
+  base_offset[0] = -base_size[0]/2.0f; base_offset[1] = -base_size[1]/2.0f; base_offset[2] = 0.0f;
   // sedan's roof is in the middle
   if (type == 1)
-    roof_offset[0] = 0.0f;
+    roof_offset[0] = -roof_size[0]/2.0f;
   else
-    roof_offset[0] = (base_size[0]-roof_size[0])/2.0f;
-  roof_offset[1] = 0.0f; roof_offset[2] = base_size[2]+roof_size[2]/2.0f;
+    roof_offset[0] = base_size[0]/2.0f - roof_size[0];
+  roof_offset[1] = -roof_size[1]/2.0f; roof_offset[2] = base_size[2];
   // No cargo
   if (cargo_size[0] < 0.1){
     cargo_offset[0] = 0.0f; cargo_offset[1] = 0.0f; cargo_offset[2] = 0.0f;
   }
   else{
-    cargo_offset[0] = (cargo_size[0]-base_size[0])/2.0f; cargo_offset[1] = 0.0f; cargo_offset[2] = base_size[2]+cargo_size[2]/2.0f;
+    cargo_offset[0] = -base_size[0]/2.0f; cargo_offset[1] = -cargo_size[1]/2.0f; cargo_offset[2] = base_size[2];
   }
+
+  // Judge whether start point is on boarder, in case octo map give seperate thin plannar.
+  for (int i = 0; i < 3; ++i)
+    {
+      int res_100 = int(m_res*100);
+      if (int(roof_offset[i]*100) % res_100 == 0)
+        roof_offset[i] += m_res/2.0f;
+      if (int(base_offset[i]*100) % res_100 == 0)
+        base_offset[i] += m_res/2.0f;
+      if (int(cargo_offset[i]*100) % res_100 == 0)
+        cargo_offset[i] += m_res/2.0f;
+    }
 
   // insert some measurements of free cells
   //Cargo: Truck's region above landing area
-  if (cargo_size[0] > 0.1){
-    for (int x=-cargo[0]-uav_safety_margin[0]*2; x<=cargo[0]+uav_safety_margin[0]*2; ++x) {
-      for (int y=-cargo[1]-uav_safety_margin[1]*2; y<=cargo[1]+uav_safety_margin[1]*2; ++y) {
-        for (int z=-cargo[2]; z<cargo[2]+uav_safety_margin[2]*2; ++z) {
-          if (x <= cargo[0] && x >= -cargo[0] && y <= cargo[1] && y >= -cargo[1] && z <= cargo[2] )
-            continue;
-          Vector3 end_vec = rot_mat.transform(Vector3((float) x*step_value+cargo_offset[0], (float) y*step_value+cargo_offset[1], (float) z*step_value+cargo_offset[2]));
-          point3d endpoint (end_vec.x(), end_vec.y(), end_vec.z());
-          m_octree->updateNode(endpoint, true);
-        }
+  for (int x=0; x<cargo[0]; x++) {
+    for (int y=0; y<cargo[1]; y++) {
+      for (int z=0; z<cargo[2]; z++) {
+        if (x <= (cargo[0]+cargo_origin[0])/2 && x >= (cargo[0]-cargo_origin[0])/2 && y <= (cargo[1]+cargo_origin[1])/2 && y >= (cargo[1]-cargo_origin[1])/2 && z <= cargo_origin[2])
+          continue;
+        Vector3 end_vec = rot_mat.transform(Vector3((float) x*m_res+cargo_offset[0], (float) y*m_res+cargo_offset[1], (float) z*m_res+cargo_offset[2]));
+        point3d endpoint (end_vec.x(), end_vec.y(), end_vec.z());
+        m_octree->updateNode(endpoint, true);
       }
     }
   }
 
 
   // Truck's roof above drivers
-  for (int x=-roof[0]-uav_safety_margin[0]*2; x<roof[0]+uav_safety_margin[0]*2; ++x) {
-    for (int y=-roof[1]-uav_safety_margin[1]*2; y<roof[1]+uav_safety_margin[1]*2; ++y) {
-      for (int z=-roof[2]; z<roof[2]+uav_safety_margin[2]*2; ++z) {
-        if (x <= roof[0] && x >= -roof[0] && y <= roof[1] && y >= -roof[1] && z <= roof[2])
+  for (int x=0; x<roof[0]; x++) {
+    for (int y=0; y<roof[1]; y++) {
+      for (int z=0; z<roof[2]; z++) {
+        if (x <= (roof[0]+roof_origin[0])/2 && x >= (roof[0]-roof_origin[0])/2 && y <= (roof[1]+roof_origin[1])/2 && y >= (roof[1]-roof_origin[1])/2 && z <= roof_origin[2])
           continue;
-        Vector3 end_vec = rot_mat.transform(Vector3((float) x*step_value+roof_offset[0], (float) y*step_value+roof_offset[1], (float) z*step_value+roof_offset[2]));
+        Vector3 end_vec = rot_mat.transform(Vector3((float) x*m_res+roof_offset[0], (float) y*m_res+roof_offset[1], (float) z*m_res+roof_offset[2]));
         point3d endpoint (end_vec.x(), end_vec.y(), end_vec.z());
-        m_octree->updateNode(endpoint, true);
+        m_octree->updateNode(endpoint, true); // integrate 'occupied' measurement
       }
     }
   }
 
   // Truck's whole base
-  for (int x=-base[0]-uav_safety_margin[0]*2; x<=base[0]+uav_safety_margin[0]*2; ++x) {
-    for (int y=-base[1]-uav_safety_margin[1]*2; y<=base[1]+uav_safety_margin[1]*2; ++y) {
-      for (int z=-base[2]; z<base[2]+uav_safety_margin[2]*2; ++z) {
-        if (x <= base[0] && x >= -base[0] && y <= base[1] && y >= -base[1] && z <= base[2])
+  for (int x=0; x<base[0]; x++) {
+    for (int y=0; y<base[1]; y++) {
+      for (int z=0; z<base[2]; z++) {
+        if (x <= (base[0]+base_origin[0])/2 && x >= (base[0]-base_origin[0])/2 && y <= (base[1]+base_origin[1])/2 && y >= (base[1]-base_origin[1])/2 && z <= base_origin[2])
           continue;
-        Vector3 end_vec = rot_mat.transform(Vector3((float) x*step_value+base_offset[0], (float) y*step_value+base_offset[1], (float) z*step_value+base_offset[2]));
+        Vector3 end_vec = rot_mat.transform(Vector3((float) x*m_res+base_offset[0], (float) y*m_res+base_offset[1], (float) z*m_res+base_offset[2]));
         point3d endpoint (end_vec.x(), end_vec.y(), end_vec.z());
-        m_octree->updateNode(endpoint, true);
+        m_octree->updateNode(endpoint, true); // integrate 'occupied' measurement
       }
     }
   }
 
-  point3d end_pt(0, 0, 0);
-  //m_octree->updateNode(end_pt, true);
+
   m_octree->prune();
   m_octree->prune();
 
